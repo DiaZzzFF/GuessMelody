c1701bca21de0479771698a82e61a9ddbb943b6c
DiaZzzFF <dvsh.dreamer@gmail.com>
2020-08-22 17:57:10 +0300

    7.7 Добавит обработчик неавторизованного пользователя и задиспатчит данные

--- src/index.js
+++ src/index.js
@@ -10,12 +10,19 @@ import {Operation as UserOperation, ActionCreator, AuthorizationStatus} from "./
 import {createAPI} from "./api.js";
 
 
-const api = createAPI(() => {});
+const onUnauthorized = () => {
+  store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH));
+};
+
+const api = createAPI(onUnauthorized);
 
 const store = createStore(
     reducer,
     applyMiddleware(thunk.withExtraArgument(api))
 );
+
+store.dispatch(DataOperation.loadQuestions());
+store.dispatch(UserOperation.checkAuth());
 
 ReactDOM.render(
     <Provider store={store}>
